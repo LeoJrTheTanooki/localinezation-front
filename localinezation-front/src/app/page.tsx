fadcb865a37b7334aa4180d013a12eab3209efb2
LeoJrTheTanooki <leodevin4118@gmail.com>
2024-04-09 09:47:11 -0700

    Removed useNavigation from root page.tsx

--- localinezation-front/src/app/page.tsx
+++ localinezation-front/src/app/page.tsx
@@ -1,6 +1,5 @@
 import NavbarComponent from "@/app/components/NavbarComponent";
 import Homepage from "./Homepage/page";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
 
