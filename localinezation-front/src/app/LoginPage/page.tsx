0ee3bb1e6e3c2d222e21257359a2b3344d207ecc
zachdressing <zed_dressel@hotmail.com>
2024-04-09 10:31:45 -0700

    Login Page

--- localinezation-front/src/app/LoginPage/page.tsx
+++ localinezation-front/src/app/LoginPage/page.tsx
@@ -9,9 +9,9 @@ const page = () => {
         router.push(route);
     };
     return (
-        <div className="min-w-screen min-h-[90vh] flex justify-center items-center">
-            <div id="heroImg" className="bg-blue-500 w-[80%] h-[900px] flex justify-center items-center rounded-3xl">
-                <div id="loginBG" className="bg-purple-400 w-[40%] h-[55%] flex flex-col justify-around items-center text-center rounded-3xl">
+        <div className="min-w-screen min-h-[90vh] flex justify-center items-center p-24">
+            <div id="heroImg" className="bg-flagBG w-[80%] h-[75vh] flex justify-center items-center rounded-3xl">
+                <div id="loginBG" className="bg-purple-600 w-[40%] h-[80%] flex flex-col justify-around items-center text-center rounded-3xl p-12">
                     <div className="flex flex-col">
                         <div id="username" className="mb-6">
                             <label className="block mb-2 text-lg text-start font-bold text-white dark:text-white">Username</label>
@@ -20,16 +20,15 @@ const page = () => {
                         <div id="password" className="mb-6">
                             <label className="block mb-2 text-lg text-start font-bold text-white dark:text-white">Password</label>
                             <input type="text" id="large-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                            <br />
                         </div>
-                        <button className="w-64 h-12 bg-fuchsia-300 rounded-full font-bold">Login</button>
+                        <a href="" className="underline italic text-white">Having trouble logging in?</a>
                         <br/>
-                        <a href="" className="underline italic">Having trouble logging in?</a>
+                        <button className="w-64 h-12 bg-fuchsia-300 rounded-full font-bold">Login</button>
                     </div>
                     <div className=""></div>
 
                     <div>
-                        <p className="">Dont have an account?</p>
+                        <p className="text-white">Dont have an account?</p>
                         <button className="w-64 h-12 bg-fuchsia-300 rounded-full font-bold">Sign Up</button>
                     </div>
                     <div>
