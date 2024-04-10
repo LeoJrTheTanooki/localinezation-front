17de23e690bf46fe1de780299747e55b15a302ca
zachdressing <zed_dressel@hotmail.com>
2024-04-10 09:03:24 -0700

    Swapped to email

--- localinezation-front/src/app/LoginPage/page.tsx
+++ localinezation-front/src/app/LoginPage/page.tsx
@@ -19,8 +19,8 @@ const LoginPage = () => {
                         <div className="flex flex-col">
                             <h1 className="text-3xl text-white font-bold mb-2">Login</h1>
                             <div id="username" className="mb-6">
-                                <label className="block mb-2 text-lg text-start font-bold text-white dark:text-white">Username</label>
-                                <input type="text" id="large-input" className="required bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:border-gray-800 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                <label className="block mb-2 text-lg text-start font-bold text-white dark:text-white">Email</label>
+                                <input type="email" id="large-input" className="required bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg hover:border-gray-800 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                             </div>
                             <div id="password" className="mb-6">
                                 <label className="block mb-2 text-lg text-start font-bold text-white dark:text-white">Password</label>
@@ -46,8 +46,8 @@ const LoginPage = () => {
                         <div className="flex flex-col">
                             <h1 className="text-3xl text-white font-bold mb-2">Create Account</h1>
                             <div id="username" className="mb-6">
-                                <label className="block mb-2 text-lg text-start font-bold text-white dark:text-white">Username</label>
-                                <input type="text" id="large-input" className="required bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                <label className="block mb-2 text-lg text-start font-bold text-white dark:text-white">Email</label>
+                                <input type="email" id="large-input" className="required bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                             </div>
                             <div id="password" className="mb-6">
                                 <label className="block mb-2 text-lg text-start font-bold text-white dark:text-white">Password</label>
