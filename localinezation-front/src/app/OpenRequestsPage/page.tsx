0a881ea4a7ddd92b71a62c884f2f586815c720d6
LeoJrTheTanooki <leodevin4118@gmail.com>
2024-04-17 23:21:43 -0700

    Variables get listed, unsure of Vercel viability

--- localinezation-front/src/app/OpenRequestsPage/page.tsx
+++ localinezation-front/src/app/OpenRequestsPage/page.tsx
@@ -6,17 +6,54 @@ import { Button } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 
 const OpenRequestsPage = () => {
-  const RequestsDefault = {
-    openRequests: [],
-  };
+  const requestsDefault = Array<
+    [
+      {
+        requestName: "string";
+        requestDialogue: "string";
+        requestReferences: [""];
+        submittedTranslations: [
+          {
+            translatorUserName: "string";
+            isGuest: true;
+            translatedDialogue: "string";
+          }
+        ];
+      }
+    ]
+  >;
 
-  const [currentMedia, setCurrentMedia] =
-    useState<ILanguageData>(RequestsDefault);
+  const [requestsArray, setRequestsArray] =
+    useState<Array<ILanguageData["openRequests"]>>(requestsDefault);
+  const [currentRequest, setCurrentRequest] =
+    useState<ILanguageData["openRequests"]>();
+  const [requestIndex, setRequestIndex] = useState<number>(0);
   const [mediaRequests, setMediaRequests] = useState<any>(PageData);
   const [queryNum, setQueryNum] = useState<number>(-1);
   const [langQuery, setLangQuery] = useState<string>("");
-  const [requestName, setRequestName] = useState<string>("null");
+  const [requestList, setRequestList] = useState<any>();
 
+  useEffect(() => {
+    if (requestsArray.length != 0) {
+      const requestListJsx = requestsArray.map((request: any, index: any) => {
+        console.log(request.requestName);
+        return (
+          <li
+            key={index}
+            onClick={() => {
+              console.log(requestsArray[index]);
+              setRequestIndex(index);
+            }}
+          >
+            {request.requestName}
+          </li>
+        );
+      });
+      setRequestList(requestListJsx);
+    }
+  }, [requestsArray]);
+
+  // Getting queries and setting to respective variables on init
   useEffect(() => {
     const idQuery = new URLSearchParams(window.location.search).get("id");
     const langQuery = new URLSearchParams(window.location.search).get(
@@ -26,6 +63,7 @@ const OpenRequestsPage = () => {
     if (langQuery) setLangQuery(langQuery);
   }, []);
 
+  // Setting data to variables based on set query variables
   useEffect(() => {
     function findLanguage(obj: any) {
       return Object.keys(obj)[0] == langQuery;
@@ -36,20 +74,24 @@ const OpenRequestsPage = () => {
         mediaRequests[queryNum].requestLanguage.find(findLanguage)[
           `${langQuery}`
         ][0];
-      console.log(requestData);
-      setCurrentMedia(requestData);
+      console.log(requestData.openRequests);
+      setRequestsArray(requestData.openRequests);
+      // if (requestData.length != 0) {
+      //   setCurrentRequest(requestData);
+      // }
     }
   }, [queryNum, langQuery]);
 
+  // Changing data based on current index
   // useEffect(() => {
-  //   if (currentMedia.openRequests)
-  //     setRequestName(currentMedia.openRequests[0].requestName);
-  // }, [currentMedia]);
+  //   console.log(requestsArray)
+  //   setCurrentRequest()
+  // }, [requestIndex]);
 
-  useEffect(() => {
-    console.log(currentMedia);
-    // console.log(requestName);
-  }, [currentMedia, requestName]);
+  // useEffect(() => {
+  //   if (requestsArray.openRequests)
+  //     setRequestName(requestsArray.openRequests[0].requestName);
+  // }, [requestsArray]);
 
   return (
     <div className=" grid grid-cols-2 gap-5">
@@ -58,7 +100,7 @@ const OpenRequestsPage = () => {
         <div>
           <h2 className=" text-3xl">Open Requests For Title</h2>
           <ul>
-            <li>
+            {/* <li>
               <button className=" text-blue-600">Main Menu Options</button>
             </li>
             <li>
@@ -72,12 +114,26 @@ const OpenRequestsPage = () => {
             </li>
             <li>
               <button className=" text-blue-600">Opening Movie</button>
-            </li>
+            </li> */}
+
+            {requestsArray.length != 0 ? (
+              <>
+                {/* <li>Pass</li> */}
+                {requestList}
+              </>
+            ) : (
+              <li>Fail</li>
+            )}
           </ul>
         </div>
       </div>
       <div>
-        <h2 className=" text-3xl">{requestName}</h2>
+        <h2 className=" text-3xl">
+          {requestsArray.length != 0
+            ? // Unsure of how to resolve errors on page, page itself loads fine on local, unsure of Vercel
+              requestsArray[requestIndex]?.requestName
+            : "null"}
+        </h2>
         <iframe
           width="560"
           height="315"
@@ -96,14 +152,14 @@ const OpenRequestsPage = () => {
             Current Translators
           </div>
           <div className=" border-2 border-b-0 border-black">
-            G0dU50pp_800: Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Cras sollicitudin massa non imperdiet suscipit. Vivamus ex
-            urna, egestas sit amet mattis et, vestibulum eget metus. Quisque
-            finibus rutrum venenatis. Cras et tempus lacus. Nulla quis enim at
-            est cursus tempor ac non sapien. Phasellus elit lectus, vehicula
-            eget arcu sed, consectetur vulputate metus. Aliquam erat volutpat.
-            Proin molestie dui at dolor pulvinar pellentesque. Cras aliquam
-            justo nec dui tristique egestas.
+            {requestsArray.length != 0
+              ? // Unsure of how to resolve errors on page, page itself loads fine on local, unsure of Vercel
+                requestsArray[requestIndex]?.submittedTranslations[0]
+                  ?.translatorUserName +
+                ": " +
+                requestsArray[requestIndex]?.submittedTranslations[0]
+                  ?.translatedDialogue
+              : ""}
             <div className=" flex">
               <div className=" mr-3">User Score: </div>
               <div>Your Score: </div>
