da1c9a7ad5b0a45450095fa45b5d7f7816e8d02f
zachdressing <zed_dressel@hotmail.com>
2024-05-14 15:53:38 -0700

    css change

--- localinezation-front/src/app/components/MediaCard.tsx
+++ localinezation-front/src/app/components/MediaCard.tsx
@@ -25,19 +25,21 @@ const MediaCard = (props: IProp) => {
         className="h-96 w-80 flex flex-col bg-purple-600 hover:bg-purple-900 text-white text-center rounded-lg border border-gray-200 dark:border-gray-700"
         onClick={() => handlePageChange(`/MediaPage?id=${props.index}`)}
       >
-        <div className="flex h-fit min-w-80 flex-col justify-center gap-2 p-6">
+        <div className="flex min-w-80 h-full flex-col justify-around gap-2 p-6">
           <img
             className="self-center max-h-96"
             style={{ maxHeight: "250px" }}
             src={props.mediaObject.coverArt}
             alt={props.mediaObject.title + " cover"}
           />
-          <p className=" mt-3 font-bold">{props.mediaObject.title}</p>
+          <div>
+          <p className=" mt-3 mb-2 font-bold">{props.mediaObject.title}</p>
           <p>
             {props.mediaObject.originalLanguage +
               " | " +
               props.mediaObject.type}
           </p>
+          </div>
         </div>
       </div>
     </div>
