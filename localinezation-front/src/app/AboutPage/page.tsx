45c61c37db8dbf45aa01cf8b10dc62184b506ba9
LeoJrTheTanooki <leodevin4118@gmail.com>
2024-05-15 13:47:35 -0700

    Adjusted AboutPage styling

--- localinezation-front/src/app/AboutPage/page.tsx
+++ localinezation-front/src/app/AboutPage/page.tsx
@@ -3,54 +3,54 @@
 import React from "react";
 
 const AboutPage = () => {
-  //   const englishText = [
-  //     <h1 className="text-center font-bold text-gray-700 text-4xl py-7">
-  //       About Us
-  //     </h1>,
-  //     <p>What is LocaLINEzation?</p>,
-  //     <p className="indent-8">
-  //       LocaLINEzation is a community driven website dedicated to translating
-  //       parts of foreign media for fans who may not know said language. We use an
-  //       open response system in order to allow for any and all potential
-  //       translatiors to find where their expertise can be used, creating a direct
-  //       LINE between the requester and the translating community.
-  //     </p>,
-  //     <p>How to use LocaLINEzation</p>,
-  //     <>
-  //       <p className="indent-8 mb-8">
-  //         If you want to <span className="font-bold">REQUEST</span> a media to be
-  //         submitted, first check the recent translations page or use the search
-  //         bar to see if that media has already been requested before, and if it
-  //         hasnt then use the <span className="font-bold">SUBMIT A MEDIA</span>{" "}
-  //         page.
-  //       </p>
-  //       <p className="indent-8">
-  //         If you are looking to <span className="font-bold">TRANSLATE</span>, make
-  //         sure that you have correctly selected your origin language and language
-  //         to translate to, located to the left of your name in the top corner of
-  //         the page, and then find something to translate!
-  //       </p>
-  //     </>,
-  //     <p>Why is LocaLINEzation?</p>,
-  //     <p className="indent-8">
-  //       LocaLINEzation was made to help break the langauge barrier between foreign
-  //       fans of any given media. While it doesn&rsquo;t provide full translations,
-  //       it helps to make cracks into a given media&rsquo;s language barrier in
-  //       hopes of eventually leading to a full translation and/or localization of
-  //       said media. Individuals are also able to participate in the translation
-  //       while improving their own skillsets in the process, and potentially
-  //       getting a vital role in a full translation.
-  //     </p>,
-  //     <p>Why does this site look like this?</p>,
-  //     <p className="indent-8">
-  //       We&apos;re capturing a 2000s - 2010s web forum style with our website.
-  //       Since our target users may reminisce about that era, we&apos;re trying to
-  //       foster that energy within our website so they may have a pleasant viewing
-  //       experience. In other words, this website looks outdated by design.
-  //     </p>,
-  //   ];
-
   const englishText = [
+    <h1 className="text-center font-bold text-gray-700 text-4xl py-7">
+      About Us
+    </h1>,
+    <p>What is LocaLINEzation?</p>,
+    <p className="indent-8">
+      LocaLINEzation is a community driven website dedicated to translating
+      parts of foreign media for fans who may not know said language. We use an
+      open response system in order to allow for any and all potential
+      translatiors to find where their expertise can be used, creating a direct
+      LINE between the requester and the translating community.
+    </p>,
+    <p>How to use LocaLINEzation</p>,
+    <>
+      <p className="indent-8 mb-8">
+        If you want to <span className="font-bold">REQUEST</span> a media to be
+        submitted, first check the recent translations page or use the search
+        bar to see if that media has already been requested before, and if it
+        hasnt then use the <span className="font-bold">SUBMIT A MEDIA</span>{" "}
+        page.
+      </p>
+      <p className="indent-8">
+        If you are looking to <span className="font-bold">TRANSLATE</span>, make
+        sure that you have correctly selected your origin language and language
+        to translate to, located to the left of your name in the top corner of
+        the page, and then find something to translate!
+      </p>
+    </>,
+    <p>Why is LocaLINEzation?</p>,
+    <p className="indent-8">
+      LocaLINEzation was made to help break the langauge barrier between foreign
+      fans of any given media. While it doesn&rsquo;t provide full translations,
+      it helps to make cracks into a given media&rsquo;s language barrier in
+      hopes of eventually leading to a full translation and/or localization of
+      said media. Individuals are also able to participate in the translation
+      while improving their own skillsets in the process, and potentially
+      getting a vital role in a full translation.
+    </p>,
+    <p>Why does this site look like this?</p>,
+    <p className="indent-8">
+      We&apos;re capturing a 2000s - 2010s web forum style with our website.
+      Since our target users may reminisce about that era, we&apos;re trying to
+      foster that energy within our website so they may have a pleasant viewing
+      experience. In other words, this website looks outdated by design.
+    </p>,
+  ];
+
+  const spanishText = [
     <h1 className="text-center font-bold text-gray-700 text-4xl py-7">
       Sobre Nosotros
     </h1>,
@@ -64,20 +64,6 @@ const AboutPage = () => {
     </p>,
     <p>Como utilizar LocaLINEzation</p>,
     <>
-      {/* <p className="indent-8 mb-8">
-        If you want to <span className="font-bold">REQUEST</span> a media to be
-        submitted, first check the recent translations page or use the search
-        bar to see if that media has already been requested before, and if it
-        hasnt then use the <span className="font-bold">SUBMIT A MEDIA</span>{" "}
-        page.
-      </p>
-      <p className="indent-8">
-        If you are looking to <span className="font-bold">TRANSLATE</span>, make
-        sure that you have correctly selected your origin language and language
-        to translate to, located to the left of your name in the top corner of
-        the page, and then find something to translate!
-      </p> */}
-
       <p className="indent-8 mb-8">
         Si usted quiere <span className="font-bold">SOLICITAR</span> un medio
         para ser enviado, primero fijase en la página de traducciones recientes
@@ -113,55 +99,46 @@ const AboutPage = () => {
   ];
 
   return (
-    <div className="flex flex-col h-fit justify-center items-center my-8">
+    <div className="flex flex-col h-fit justify-center items-center py-10">
       <div
         id="pageHeader"
-        className="flex items-center h-24 bg-fuchsia-300 p-12 mx-auto rounded-lg mt-2 mb-4"
+        className="flex items-center h-24 bg-fuchsia-300 p-12 mx-auto rounded-lg mt-2 mb-10 border border-black"
       >
         {englishText[0]}
       </div>
-      <div className="w-[80vw] flex flex-col border-black border-2 my-4">
-        <div
-          id="titleHeader"
-          className="bg-purple-600 text-white p-6 text-3xl font-bold"
-        >
-          {englishText[1]}
+
+      <div className="rounded-lg text-gray-200 font-semibold py-8 max-w-[1440px] w-2/3 min-w-min flex flex-col gap-y-6">
+        <div className="bg-purple-600 p-8 flex flex-col items-center rounded-xl">
+          <div className="bg-fuchsia-300 text-3xl h-fit w-full min-w-64 text-center text-gray-700 py-3 font-semibold border-black border">
+            {englishText[1]}
+          </div>
+          <div className="border border-t-0 h-fit w-full min-w-64 border-black flex flex-col flex-wrap p-4 bg-fuchsia-200 text-gray-700">
+            {englishText[2]}
+          </div>
         </div>
-        <div id="textBox" className="p-6 text-xl">
-          {englishText[2]}
+        <div className="bg-purple-600 p-8 flex flex-col items-center rounded-xl">
+          <div className="bg-fuchsia-300 text-3xl h-fit w-full min-w-64 text-center text-gray-700 py-3 font-semibold border-black border">
+            {englishText[3]}
+          </div>
+          <div className="border border-t-0 h-fit w-full min-w-64 border-black flex flex-col flex-wrap p-4 bg-fuchsia-200 text-gray-700">
+            {englishText[4]}
+          </div>
         </div>
-      </div>
-      <div className="w-[80vw] flex flex-col border-black border-2 my-4">
-        <div
-          id="titleHeader"
-          className="bg-purple-600 text-white p-6 text-3xl font-bold"
-        >
-          {englishText[3]}
+        <div className="bg-purple-600 p-8 flex flex-col items-center rounded-xl">
+          <div className="bg-fuchsia-300 text-3xl h-fit w-full min-w-64 text-center text-gray-700 py-3 font-semibold border-black border">
+            {englishText[5]}
+          </div>
+          <div className="border border-t-0 h-fit w-full min-w-64 border-black flex flex-col flex-wrap p-4 bg-fuchsia-200 text-gray-700">
+            {englishText[6]}
+          </div>
         </div>
-        <div id="textBox" className="p-6 text-xl">
-          {englishText[4]}
-        </div>
-      </div>
-      <div className="w-[80vw] flex flex-col border-black border-2 my-4">
-        <div
-          id="titleHeader"
-          className="bg-purple-600 text-white p-6 text-3xl font-bold"
-        >
-          {englishText[5]}
-        </div>
-        <div id="textBox" className="p-6 text-xl">
-          {englishText[6]}
-        </div>
-      </div>
-      <div className="w-[80vw] flex flex-col border-black border-2 my-4">
-        <div
-          id="titleHeader"
-          className="bg-purple-600 text-white p-6 text-3xl font-bold"
-        >
-          {englishText[7]}
-        </div>
-        <div id="textBox" className="p-6 text-xl">
-          {englishText[8]}
+        <div className="bg-purple-600 p-8 flex flex-col items-center rounded-xl">
+          <div className="bg-fuchsia-300 text-3xl h-fit w-full min-w-64 text-center text-gray-700 py-3 font-semibold border-black border">
+            {englishText[7]}
+          </div>
+          <div className="border border-t-0 h-fit w-full min-w-64 border-black flex flex-col flex-wrap p-4 bg-fuchsia-200 text-gray-700">
+            {englishText[8]}
+          </div>
         </div>
       </div>
     </div>
