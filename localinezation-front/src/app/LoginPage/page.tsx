1fb16d688b9c381d13a2f3da408ac8f05a501c1e
zachdressing <zed_dressel@hotmail.com>
2024-05-23 14:41:30 -0700

    There was a random 3 on the page, checking branchs

--- localinezation-front/src/app/LoginPage/page.tsx
+++ localinezation-front/src/app/LoginPage/page.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
 const LoginPage = () => {
   const [modalMessage, setModalMessage] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
-  3
+  
   const Modal: React.FC<ModalProps> = ({ isOpen, message, onClose }) => {
     if (!isOpen) return null;
 
